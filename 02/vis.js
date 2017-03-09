bcbe7906a704f8e2f440275a31d81cd99a459de3
Micah Stubbs <micah.stubbs@gmail.com>
2017-03-09 13:54:59 -0800

    improve indentation for drag functions

--- 02/vis.js
+++ 02/vis.js
@@ -144,17 +144,17 @@ d3.json('graph.json', (error, graph) => {
     .call(makeAnnotations);
 });
 
-    function dragstarted(d) {
-      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
-    }
-    function dragged(d) {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
-    }
-    function dragended(d) {
-      if (!d3.event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
-    }
+function dragstarted(d) {
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+function dragged(d) {
+  d.fx = d3.event.x;
+  d.fy = d3.event.y;
+}
+function dragended(d) {
+  if (!d3.event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
